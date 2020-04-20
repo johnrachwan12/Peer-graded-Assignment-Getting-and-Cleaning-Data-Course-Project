fceed546166319a0e02005d12c198c6968c6cf4b
johnrachwan12 <63955871+johnrachwan12@users.noreply.github.com>
2020-04-20 09:42:45 +0300

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,75 @@
+
+
+if(!file.exists("./data")){dir.create("./data")}
+#Here are the data for the project:
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip")
+
+# Unzip dataSet to /data directory
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+
+#You should create one R script called run_analysis.R that does the following.
+
+#trainings tables
+
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+
+#testing tables
+x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+
+# vector
+features <- read.table('./data/UCI HAR Dataset/features.txt')
+
+# activity labels:
+activityLabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
+
+#  column 
+
+colnames(x_train) <- features[,2]
+colnames(y_train) <-"activityId"
+colnames(subject_train) <- "subjectId"
+
+colnames(x_test) <- features[,2] 
+colnames(y_test) <- "activityId"
+colnames(subject_test) <- "subjectId"
+
+colnames(activityLabels) <- c('activityId','activityType')
+
+#Merging them
+
+mrg_train <- cbind(y_train, subject_train, x_train)
+mrg_test <- cbind(y_test, subject_test, x_test)
+setAllInOne <- rbind(mrg_train, mrg_test)
+
+#[1] 10299   563
+
+#Reading 
+
+colNames <- colnames(setAllInOne)
+
+#defining ID, mean and standard deviation
+
+mean_and_std <- (grepl("activityId" , colNames) | 
+                   grepl("subjectId" , colNames) | 
+                   grepl("mean.." , colNames) | 
+                   grepl("std.." , colNames) 
+)
+
+#Making  subset 
+
+setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
+setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
+                              by='activityId',
+                              all.x=TRUE)
+
+#second tidy 
+
+secTidySet <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
+secTidySet <- secTidySet[order(secTidySet$subjectId, secTidySet$activityId),]
+
+write.table(secTidySet, "second_tidy.txt", row.name=FALSE)
+
